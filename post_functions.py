44715db5b9cea96bb894a46a1c62538777efedc2
Mark Lysenko <58804992+SuperLalka@users.noreply.github.com>
2020-04-14 18:20:21 +0300

    First step to pagination

--- post_functions.py
+++ post_functions.py
@@ -1,9 +1,13 @@
+def list_pages(posts_num):
+    if posts_num % 10 == 0:
+        pages = range(1, (posts_num // 10) + 1)
+    else:
+        pages = range(1, (posts_num // 10) + 2)
+    return list(pages)
+
 def post_info():
     from post_list import posts
-    post = []
-    for line in posts:
-        post.append(line)
-    return post
+    return posts
 
 def get_post(key, file):
     post = []
--- titanic.py
+++ titanic.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-from post_functions import post_info, get_post
+from post_functions import post_info, get_post, list_pages
 
 app = Flask(__name__)
 
-@app.route('/')                                             
+@app.route('/')
+@app.route('/<int:page>')
 def content_page() -> 'html':
     info = post_info()
+    pages = list_pages(len(info))
     return render_template('root_page.html',
-                           the_info = info)
+                           the_info = info,
+                           the_pages = pages)
 
                                
 @app.route('/post/<name>')
